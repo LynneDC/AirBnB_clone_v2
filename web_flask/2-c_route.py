dc0b262f92b23508f79f740dda14546f4f1ed6d5
LynneDc <roselinedc1308@gmail.com>
2023-10-20 14:53:31 +0200

    routing with viriable added and replace to replace _ with space.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" mport Flask module form flask
+python script that display “Hello HBNB!”
+and /hbnb: display "HBNB"
+/c/<text>: display “C ” followed by the value of the text variable
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Returns text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    """ Return text. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
